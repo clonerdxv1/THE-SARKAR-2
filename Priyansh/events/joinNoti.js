13e66f2f31ad971283e4422521308b102a653629
𝐒𝐀𝐑𝐃𝐀𝐑 𝐑𝐃𝐗 🩷🪽 <clonerdx2@gmail.com>
2025-07-14 18:08:49 +0500

    Update joinNoti.js

--- Priyansh/events/joinNoti.js
+++ Priyansh/events/joinNoti.js
@@ -50,7 +50,7 @@ ${global.config.PREFIX}🔧💬 For any command issue or query,
 ╔════════════╗
     A⃠K⃠A⃠S⃠H⃠
 ╚════════════╝
-`, attachment: fs.createReadStream(__dirname + "/cache/botjoining.mp4")} ,threadID));
+`, attachment: fs.createReadStream(__dirname + "/cache/botjoinig.mp4")} ,threadID));
         }
         else {
                 try {
