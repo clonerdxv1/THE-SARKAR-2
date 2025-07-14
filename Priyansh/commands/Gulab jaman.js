516708f83b2c25c91450e9d95cca0982b36b8e86
𝐒𝐀𝐑𝐃𝐀𝐑 𝐑𝐃𝐗 🩷🪽 <clonerdx2@gmail.com>
2025-07-14 14:43:50 +0500

    Update Gulab jaman.js

--- Priyansh/commands/Gulab jaman.js
+++ Priyansh/commands/Gulab jaman.js
@@ -33,7 +33,7 @@ module.exports.run = async ({ api, event, args, Users, Threads, Currencies }) =>
   api.sendMessage("𝗝𝗜 𝗝𝗜 𝗚𝗨𝗟𝗔𝗕 𝗝𝗔𝗠𝗨𝗡 𝗞𝗜 𝗕𝗛𝗢𝗢𝗞 𝗟𝗔𝗚 𝗚𝗔𝗘𝗬 𝗛𝗔𝗜 𝗝𝗡𝗔𝗕 𝗞𝗢 𝗗𝗘𝗧𝗔 𝗛𝗨 𝗚𝗨𝗟𝗔𝗕 𝗝𝗔𝗠𝗨𝗡", event.threadID, () => {
     // Once the initial message is sent, proceed to send the image
     var callback = () => api.sendMessage(
-      { body: `MADE BY RDX: ${link.length}`, attachment: fs.createReadStream(__dirname + "/cache/1.jpg") },
+      { body: `ᱪ❤️ᱪ 𝘼𝙆 ᱪ❤️ᱪ: ${link.length}`, attachment: fs.createReadStream(__dirname + "/cache/1.jpg") },
       event.threadID,
       () => fs.unlinkSync(__dirname + "/cache/1.jpg"),
       event.messageID
